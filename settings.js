f93ab721e384959449b6377fc5d287193095ac3c
bhagatdeepsingh <bhagatdeepsingh90@gmail.com>
2025-04-14 12:36:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "FatalityX",
+  botOwner: 'Bhagatdeep Singh', // Your name
+  ownerNumber: '7055531530', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Made with 💖 by Bhagatdeep Singh...!",
   version: "1.0.0",
 };
 
